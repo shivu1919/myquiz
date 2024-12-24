2b50d290421860552a2a638d1ee2322a319a1ab6
shivu1919 <shivamthakur.ss129@gmail.com>
2024-12-24 18:56:30 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './App.css'
+import Final from './Final'
 
 function App() {
 
@@ -45,40 +46,80 @@ function App() {
       }
     ]
 
+    
 
     const[counter, setCounter] = useState(0)
-
-
+    const[score, setScore] = useState(0)
+   
+    let temp;
     function CheckAnswer(index){
-      console.log("This function is working");
       
-      console.log(index)
+      if(options[counter].optionvalue[index]=="Narendra Modi"){
+        temp = score+1
+        setScore(temp)
+        console.log(temp)
+        console.log("second if")
+      }
+      
+      if(options[counter].optionvalue[index]=="New Delhi"){
+      temp = score+1
+        setScore(temp)
+        console.log(temp)
+        console.log("first if")
+      }
+
+      if(options[counter].optionvalue[index]=="Draupadi Murmu"){
+         temp = score+1
+        setScore(temp)
+        console.log(temp)
+        console.log("third if")
+      }
+
+      if(options[counter].optionvalue[index]=="Amrawati"){
+       temp = score+1
+        setScore(temp)
+        console.log(temp)
+        console.log("fourth if")
+      }
     }
 
-    function Change(){
-      CheckAnswer()
+   
+    const Change=(index)=>{
+      CheckAnswer(index);
       setCounter(counter+1)
       
     }
 
-
-  return (
-    <div className="container">
-      <h1>My Quiz Application</h1>
-        <div className="card">
-            <h2>Question No: {counter+1} /4</h2>
-
-            <h2>{question[counter]}</h2>
-            
-            <div id="option">
-              {options[counter].optionvalue.map((x,index)=> <li key={index} onClick={()=>{Change(index)}} id={index}>{x}</li>)}
-
+   
+    if(counter<=3){
+      return (
+        <div className="container">
+          <h1>My Quiz Application</h1>
+            <div className="card">
+                <h2>Question No: {counter+1} /4</h2>
+    
+                <h2>{question[counter]}</h2>
+                
+                <div id="option">
+                  {options[counter].optionvalue.map((x,index)=> <li key={index} onClick={()=>{Change(index)}} id={index}>{x}</li>)}
+    
+                </div>
+                
+                <br />
             </div>
-            
-            <br />
         </div>
-    </div>
-  )
+      )
+    }
+    else{
+      return(
+        <>
+          <Final />
+          <h1 id="answer">Your score is : {score} / 4</h1>
+        </>
+      );
+    }
+
+  
 }
 
 export default App
---
+++ src/Final.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function Final() {
+  return (
+    <>
+      <div className="final">
+          <h1>Your Quiz Over</h1>
+        </div>   
+    </>
+  
+  )
+}
+
+export default Final
